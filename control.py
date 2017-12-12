3aa28f943143fe69be8b3e42f688162b77125bfd
Olya145 <31884547+Olya145@users.noreply.github.com>
2017-12-12 14:46:58 +0300

    Update control.py

--- control.py
+++ control.py
@@ -2,8 +2,8 @@ with open ("C:/Users/student/Desktop/Ozhegov.txt", encoding = "utf-8") as f:
     lines = f.readlines()
     for line in lines:
         word = line.split("|")
-        if len(word) >= 20: 
-                print(lines)
+        if len(word[0]) >= 20: 
+                print(line)
         
 
 
